9ab647312410d2a2621f0996347212dc25690633
Madeep9347 <111452217+Madeep9347@users.noreply.github.com>
2024-02-28 11:51:14 +0530

    Update remote_backend_s3.tf

--- infra/remote_backend_s3.tf
+++ infra/remote_backend_s3.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "dev-proj-1-remote-state-bucket-12345"
+    bucket = "dev-proj-1-remote-state-bucket-123"
     key    = "devops-project-1/terraform.tfstate"
     region = "eu-central-1"
   }
